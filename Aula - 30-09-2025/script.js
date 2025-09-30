1aadeb995af22c23df24a9135079d7a31be68ecb
maatheusZK <maatheusseveriano@gmail.com>
2025-09-30 15:30:11 -0300

    Add files via upload

---
+++ Aula - 30-09-2025/script.js
@@ -0,0 +1,20 @@
+// arrays -> vetores, matrizes
+// estrutura de dados para armazenas um conjunto de elementos
+
+let frutas = ["Banana", "Maça", "Melancia"]; //array tamanho 3 
+console.log(frutas);
+console.log(frutas[1]);
+
+let numeros =  new Array(10,2,6,7);
+console.log(numeros);
+
+let arrayVazio = [];
+console.log(arrayVazio);
+arrayVazio[0] = "Carlos";
+console.log(arrayVazio);
+
+// Tamanho de array -> length
+
+let dados = [32,4,80,6]; // Tamanho 5 -> length 5
+console.log(dados.length);
+ 
